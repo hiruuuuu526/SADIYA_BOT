425069fc339cee8d26ac7253b64038f7bb9f100e
hiruuuuu526 <Sadiyahiru@gmail.com>
2024-11-28 09:38:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ if(!isOwner && !isGroup && config.MODE === "groups") return
 //=========OWNER - REACTION ===============================  
 if(senderNumber.includes("94726840185")){
 if(isReact) return
-m.react("🧑‍🔧")
+m.react("🐼")
 }
 //=====================✓
        
